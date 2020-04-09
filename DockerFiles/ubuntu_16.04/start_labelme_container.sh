c3aa9597d799067d5c445371ed19aa932e4b203a
GuoLiuFang <2453454795@qq.com>
2020-04-09 16:57:59 +0000

    update the start up script

--- DockerFiles/ubuntu_16.04/start_labelme_container.sh
+++ DockerFiles/ubuntu_16.04/start_labelme_container.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 docker run \
---name labelme \
--p 8080:80 \
+--name labelme_trade_ai \
+-p 8998:80 \
+-p 8999:81 \
+-p 8997:82 \
+-v /LegendStart/LegendStartDataManagement/Temp:/DataHub/Temp \
 -d \
 --entrypoint "/bin/bash" \
 -t labelme
 
 # restart apache inside the container
-docker exec labelme service apache2 restart
+docker exec labelme_trade_ai service apache2 restart
